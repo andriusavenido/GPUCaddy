d8bddbbdb9ea5b51c346a7a49f243ae9347b28ac
andriusavenido <andriusavenido@gmail.com>
2025-03-24 11:21:34 -0400

    api maven setup

---
+++ CaddyAPI/src/main/java/dev/avenido/CaddyAPI/CaddyApiApplication.java
@@ -0,0 +1,13 @@
+package dev.avenido.CaddyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaddyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaddyApiApplication.class, args);
+	}
+
+}
---
+++ CaddyAPI/src/test/java/dev/avenido/CaddyAPI/CaddyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.avenido.CaddyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaddyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
