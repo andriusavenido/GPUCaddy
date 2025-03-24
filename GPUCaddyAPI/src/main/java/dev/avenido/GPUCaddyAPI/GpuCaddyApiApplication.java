1b6e5b0817b4d427c9ea810a5ec37d85e5760c71
andriusavenido <andriusavenido@gmail.com>
2025-03-24 11:15:50 -0400

    setup

---
+++ GPUCaddyAPI/src/main/java/dev/avenido/GPUCaddyAPI/GpuCaddyApiApplication.java
@@ -0,0 +1,13 @@
+package dev.avenido.GPUCaddyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpuCaddyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpuCaddyApiApplication.class, args);
+	}
+
+}
---
+++ GPUCaddyAPI/src/test/java/dev/avenido/GPUCaddyAPI/GpuCaddyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.avenido.GPUCaddyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpuCaddyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
